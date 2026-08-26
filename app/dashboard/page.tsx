52a082bc9e3fd9ee4951fcfc7010dcd5ac1342e9
Nathan-s-Anthony <nsanthony95@gmail.com>
2026-08-26 19:54:22 +0200

    mobile responsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function Dashboard() {
     return (
         <div className="grid h-screen relative overflow-hidden w-screen grid-cols-[250px_1fr] grid-rows-[auto_1fr]">
             <ToolBar />
-            <div className="min-w-0 col-start-2 z-10  lg:px-6 py-4">
+            <div className="min-w-0  col-span-2 lg:col-start-2 z-10  lg:px-6 py-4">
                 <Panel heading="Your stats" />
             </div>
         </div>
