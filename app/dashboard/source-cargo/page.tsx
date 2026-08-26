347576da4981f7d1860d15bd9dee420a0b3741bb
Nathan-s-Anthony <nsanthony95@gmail.com>
2026-08-26 23:44:27 +0200

    latest updates

--- app/dashboard/source-cargo/page.tsx
+++ app/dashboard/source-cargo/page.tsx
@@ -7,8 +7,7 @@ export default function SourceCargo({ imageSrc, className }: { imageSrc: string,
         <Panel heading="Source Cargo" subHeading="Get a bang for your buck" className="">
             <h2 className="mb-6">Choose your retailers to source from</h2>
             <div>
-                {/* <Card /> */}
-                <div className={`bg-[#fd0] relative w-40 h-40 flex items-center p-4`}>
+                <div className={`bg-[#fd0] rounded-sm relative w-40 h-40 flex items-center p-4 cursor-pointer`}>
                     <Image src={"/makroLogo.png"} alt="" width={250} height={250} className="" />
                 </div>
             </div>
