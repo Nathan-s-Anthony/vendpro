f2da33ee9e816358c1c51173d5fd06f36f7255ed
Nathan-s-Anthony <nsanthony95@gmail.com>
2026-08-27 09:02:59 +0200

    del file

--- app/utils/date.ts
+++
@@ -1,7 +0,0 @@
-    //south african only , dont need to check other timezones
-    export const dateDayMonthYear = new Date().toLocaleDateString("en-ZA", {
-        timeZone: "Africa/Johannesburg",
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-    });
